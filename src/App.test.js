20d511f0095853016fb987eacca094bc5a5e32c2
Pavel Kharytonau <58185330+pkmsq@users.noreply.github.com>
2022-08-26 10:40:07 +0300

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
-test('Test One', () => {
-    expect(true).toBeTruthy();
+import { add } from './App';
+
+test('add', () => {
+
+    const value = add(1,2);
+
+    expect(value).toBe(3);
 });
 
-test('Test Two', () => {
-    expect(true).toBeTruthy();
-});
+// test('Test Two', () => {
+//     expect(true).toBeTruthy();
+// });
